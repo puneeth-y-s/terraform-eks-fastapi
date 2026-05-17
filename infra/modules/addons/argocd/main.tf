baa0ae67d993220eae5347d1a6ce7666fc4de9a3
puneeth-y-s <yspuneeth1994@gmail.com>
2026-05-17 14:33:02 +0530

    Added ArgoCD and ArgoCD Image Updater

---
+++ infra/modules/addons/argocd/main.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "argocd" {
+
+    name = "argocd"
+
+    repository = "https://argoproj.github.io/argo-helm"
+    chart = "argo-cd"
+    namespace = "argocd"
+    create_namespace = true
+    version = "7.3.11"
+
+    values = [file("values/argocd.yaml")]
+}
